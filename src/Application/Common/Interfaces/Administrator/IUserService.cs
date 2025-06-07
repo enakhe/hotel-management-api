dd9c0a0807c42499eb8abe9f8f3ace609e2a672f
Enakhe <izuagbesam@gmail.com>
2025-06-07 18:36:21 +0100

    Add the IUserService interface and method signatures

---
+++ src/Application/Common/Interfaces/Administrator/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelManagement.Application.Common.DTOs.Administrator;
+
+namespace HotelManagement.Application.Common.Interfaces.Administrator;
+public interface IUserService
+{
+    Task<Guid> CreateUserAsync(CreateUserDto dto);
+    Task UpdateUserAsync(UpdateUserDto dto);
+    Task DeactivateUserAsync(Guid userId);
+    Task DeleteUserAsync(Guid userId);
+    Task<UserDto> GetUserByIdAsync(Guid userId);
+    Task<IEnumerable<UserDto>> GetAllUsersAsync();
+
+    Task AssignRolesAsync(Guid userId, List<Guid> roleIds);
+    Task<List<string>> GetUserRolesAsync(Guid userId);
+
+    Task ResetPasswordAsync(Guid userId, string newPassword);
+
+    Task<bool> EmailExistsAsync(string email);
+    Task<bool> PhoneExistsAsync(string phoneNumber);
+}
