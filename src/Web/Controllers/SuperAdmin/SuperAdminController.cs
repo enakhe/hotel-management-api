cb33f06a5a7317e22923a05c527f9d2975b7586b
Enakhe <izuagbesam@gmail.com>
2025-10-12 01:03:14 +0100

    Add a new endpoint for admin to login

---
+++ src/Web/Controllers/SuperAdmin/SuperAdminController.cs
@@ -0,0 +1,28 @@
+﻿using HotelManagement.Application.Common.Interfaces.Auth;
+using HotelManagement.Application.Core.Auth.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Controllers.SuperAdmin;
+
+/// <summary>
+/// SuperAdmin main controller
+/// </summary>
+[ApiController]
+[Route("cp")]
+[Authorize(Roles = "SuperAdmin")]
+public class SuperAdminController(ISender mediator, ILogger<SuperAdminController> logger) : ControllerBase
+{
+    private readonly ISender _mediator = mediator;
+    private readonly ILogger<SuperAdminController> _logger = logger;
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Login([FromBody] LoginCommand command)
+    {
+        var response = await _mediator.Send(command);
+
+        return !response.Succeeded ? StatusCode(response.StatusCode, response) : (ActionResult)Ok(response);
+    }
+}
